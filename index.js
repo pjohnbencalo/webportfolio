d481e8b4335d1a5d85a8dae37cec33b9360fb864
Philip John Bencalo <pj.bencalo@gmail.com>
2023-05-19 20:44:06 +0800

    Final output

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let cardFooterText = document.getElementsByClassName("card-footer-text"); 
+let cardFooterText = document.getElementsByClassName("card-footer-text");
 
 const postedDate = moment([2023, 4, 17]);
 const updatedPostedDate = postedDate.add(22, "hours").add(26, "minutes");
@@ -6,3 +6,17 @@ const updatedPostedDate = postedDate.add(22, "hours").add(26, "minutes");
 cardFooterText[0].innerText = `Last updated ${updatedPostedDate.fromNow()}`;
 cardFooterText[1].innerText = `Last updated ${updatedPostedDate.fromNow()}`;
 cardFooterText[2].innerText = `Last updated ${updatedPostedDate.fromNow()}`;
+
+$(window).on("load", function () {
+  setTimeout(function () {
+    $(".loader-wrapper").addClass("fade-out");
+    $("body").removeClass("preload");
+    $(".navbar").removeClass("d-none");
+  }, 5000);
+});
+
+$(window).on("load", function () {
+  setTimeout(function () {
+    $(".loader-wrapper").hide();
+  }, 5500);
+});
